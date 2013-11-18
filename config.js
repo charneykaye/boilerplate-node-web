1022c736ab58fdff89c73170b6381516203f00f1
outrightmental <nick.c.kaye@gmail.com>
2013-11-18 02:26:39 -0500

    Drywall https://www.pivotaltracker.com/story/show/60914686
    Merge remote-tracking branch 'boilerplate/master'
    
    Conflicts:
    	.gitignore
    	LICENSE
    	README.md
    	app.js
    	package.json

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ exports.port = process.env.PORT || 3000;
 exports.mongodb = {
   uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/drywall'
 };
-exports.companyName = 'Acme, Inc.';
-exports.projectName = 'Drywall';
-exports.systemEmail = 'your@email.addy';
-exports.cryptoKey = 'k3yb0ardc4t';
+exports.companyName = 'Outright Mental Inc.';
+exports.projectName = 'Outright Mental Web Application';
+exports.systemEmail = 'hello@outrightmental.com';
+exports.cryptoKey = 'pNGQ8ypFL98agjYdtRD7ABRFQyRhn3NAHv5rSyGMzNuE3wNk2TMChnWWM3cH5sj3';
 exports.smtp = {
   from: {
-    name: process.env.SMTP_FROM_NAME || exports.projectName +' Website',
-    address: process.env.SMTP_FROM_ADDRESS || 'your@email.addy'
+    name: process.env.SMTP_FROM_NAME || exports.projectName,
+    address: process.env.SMTP_FROM_ADDRESS || 'do-not-reply@outrightmental.com'
   },
   credentials: {
-    user: process.env.SMTP_USERNAME || 'your@email.addy',
-    password: process.env.SMTP_PASSWORD || 'bl4rg!',
+    user: process.env.SMTP_USERNAME || 'do-not-reply@outrightmental.com',
+    password: process.env.SMTP_PASSWORD || 'Hv5rSyGMzNuE3wNk',
     host: process.env.SMTP_HOST || 'smtp.gmail.com',
     ssl: true
   }
